c2e8dc6484d702f05988d8e09ee482d117649d92
Remy <remy12.salem@epitech.eu>
2022-11-21 11:39:21 +0100

    adding user id to log

--- src/Middleware/Logger.go
+++ src/Middleware/Logger.go
@@ -1,11 +1,20 @@
 package Middleware
 
 import (
+	"strconv"
+
+	database "github.com/Eznopot/RM_api/src/Database"
 	logger "github.com/Eznopot/RM_api/src/Logger"
 	"github.com/gin-gonic/gin"
 )
 
 func CustomLogger(c *gin.Context) {
-	logger.Info(c.Request.Method, c.Request.URL.Path)
+	token, exist := c.Request.Header["Token"]
+	if !exist {
+		logger.Info(c.Request.Method, c.Request.URL.Path);
+	} else {
+		_, res := database.CheckSession(token[0])
+		logger.Info(c.Request.Method, c.Request.URL.Path, "by:", strconv.Itoa(res));
+	}
 	c.Next()
 }
